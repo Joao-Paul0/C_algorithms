b6ecea920748f9fb249b480495376396ee2b74ac
Joao-Paul0 <joaopaulo.tomazaquino@hotmail.com>
2022-09-06 21:19:19 -0300

    adding the constant and format-specifiers

---
+++ constants.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	
+	// constant = fixed value that cannot be altered by the program during its execution
+	
+//	float pi = 3.14159;
+	const float PI = 3.14159;
+	
+//	PI = 1.123; 
+	
+	printf("%f", PI);
+	
+	return 0;
+	
+}
