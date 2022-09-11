f735a49ddd17f9a1852693af3ec76b5656a26af8
Joao-Paul0 <joaopaulo.tomazaquino@hotmail.com>
2022-09-10 21:36:28 -0300

    adding math-function and augmented-assignment-operators

---
+++ math-function.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	
+	double A = sqrt(9); // square root
+	double B = pow(2, 4); // raised to power
+	int C = round(3.14); // round a number
+	int D = ceil(3.14); // round up a number
+	int E = floor(3.99); // round a number down
+	double F = fabs(-100); // make them negative numbers to positive
+	double G = log(3); // logarithm
+	double H = sin(45); // sine trigonometries
+	double I = cos(45); // cosine trigonometries
+	double J = tan(45); // tagent trigonometries
+	
+	printf("\n%lf", J);
+	
+	return 0;
+}
